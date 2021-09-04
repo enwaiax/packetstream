757236c580467641e100c9673d0175db5a88bcdc
Chasing66 <chasing66@live.com>
2021-09-04 07:39:56 +0000

    Start packerstream

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
 # packetstream
+<!-- PROJECT SHIELDS -->
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <br>
+    <img src="https://packetstream.io/assets/images/logo.png?cb=1614583587" alt="PacketStream Logo" width="33" height="38">
+    <h3 align="center">PacketStream</br>
+  </br>
+  <h3 align="center">Docker image for PacketStream</h3>
+  <p align="center">分享闲置家用带宽获得利润: $0.10/GB</p>
+  <p align="center">
+    <a href="https://github.com/Chasing66/PacketStream" target="_blank">Github</a>
+    |
+    <a href="https://hub.docker.com/r/enwaiax/PacketStream" target="_blank">Docker Hub</a>
+  </p>
+</p>
+
+## MJJ
+- 请低调使用，张弛有度
+
+## 充电支持
+
+<a href="https://afdian.net/@LuckyHunter"><img src="https://img.shields.io/badge/%E7%88%B1%E5%8F%91%E7%94%B5-LuckyHunter-%238e8cd8?style=for-the-badge" alt="前往爱发电赞助" width=auto height=auto border="0" /></a>
+
+## 介绍
+- 本项目基于Alpine docker 镜像搭建PacketStream容器，实现在单个VPS上同时并发运行多个进程，可以获得多倍的流量。脚本包括自动增加虚拟内存（物理内存的两倍）、安装docker、安装docke-compose、设置CID、设置运行容器数量等。
+- 脚本会自动检测是否满足Residential IP条件，若不满足会自动删除所有容器，并退出进程
+- 若本地无实际Residential IP带宽, 支持设置Residential代理来运行
+- 可以和[peer2profit](https://github.com/Chasing66/peer2profit)同时运行
+
+## 信息
+- 本项目已经在 Ubuntu16+ 和 Debian10+上验证
+- 必须住宅IP，可使用住宅代理。如需快捷开启http代理，推荐使用[x-ui](https://github.com/Chasing66/beautiful_docker/tree/main/x-ui)docker 版本
+- 开发不易，如果你想尝试，请通过我的推荐链接注册。 [推荐链接](https://packetstream.io/?psr=2HVV)
+
+
+### 使用方法
+- 交互式
+```shell
+mkdir PacketStream && cd PacketStream
+wget -Nnv https://raw.githubusercontent.com/Chasing66/PacketStream/main/packetstream.py &>/dev/null
+chmod +x packetstream.py
+python3 packetstream.py -c "你的CID" -n "容器数量" -p "http代理"
+```
+#### 例如
+```shell
+mkdir PacketStream && cd PacketStream
+wget -Nnv https://raw.githubusercontent.com/Chasing66/PacketStream/main/packetstream.py &>/dev/null
+chmod +x packetstream.py
+python3 packetstream.py -c 2HVV -n 5 -p "http://exampele.com:27015"
+```
+
+### 免责声明
+
+本程序仅供学习了解, 非盈利目的，请于下载后 24 小时内删除, 不得用作任何商业用途, 文字、数据及图片均有所属版权, 如转载须注明来源。
+
+使用本程序必循遵守部署免责声明。使用本程序必循遵守部署服务器所在地、所在国家和用户所在国家的法律法规, 程序作者不对使用者任何不当行为负责.
+
+### 鸣谢
+- [Pony](https://peer2profit.net/)
+- a^小怪
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/Chasing66/PacketStream.svg?style=for-the-badge
+[contributors-url]: https://github.com/Chasing66/PacketStream/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/Chasing66/PacketStream.svg?style=for-the-badge
+[forks-url]: https://github.com/Chasing66/PacketStream/network/members
+[stars-shield]: https://img.shields.io/github/stars/Chasing66/PacketStream.svg?style=for-the-badge
+[stars-url]: https://github.com/Chasing66/PacketStream/stargazers
+[issues-shield]: https://img.shields.io/github/issues/Chasing66/PacketStream.svg?style=for-the-badge
+[issues-url]: https://github.com/Chasing66/PacketStream/issues
+[license-shield]: https://img.shields.io/github/license/Chasing66/PacketStream.svg?style=for-the-badge
+[license-url]: https://github.com/Chasing66/PacketStream/blob/main/LICENSE
---
+++ packetstream.py
@@ -0,0 +1,150 @@
+import os
+import subprocess
+import argparse
+import time
+
+
+def get_args():
+    # Get arguments from command line
+    parser = argparse.ArgumentParser()
+    # set container number
+    parser.add_argument('-n', '--number', help='set container number', type=int)
+    # set enviornment variables: CID
+    parser.add_argument('-c', '--cid', help='set container ID', type=str)
+    # set optional arguments: proxy
+    parser.add_argument('-p', '--proxy', help='set proxy', type=str)
+    parser.add_argument('-s', '--sponsor', help='support the author', type=bool, default=True)
+    args = parser.parse_args()
+    if not args.cid or not args.number:
+        print('Please set the CID and container number')
+        exit()
+    if args.proxy:
+        # checke whether file is exist
+        if os.path.isfile(".env"):
+            os.remove(".env")
+        with open('.env', 'w') as f:
+            f.writelines(['http_proxy=' + args.proxy + '\n' + 'https_proxy=' + args.proxy + '\n' + 'CID=' + args.cid + '\n'])
+    return args
+
+def check_run_as_root():
+    # check if the script is run as root
+    if os.getuid() != 0:
+        print('Please run this script as root')
+        exit()
+
+def create_swap_file():
+    # get the swap memory size
+    swap_size = subprocess.check_output(['free', '-h', '-w']).decode('utf-8').split('\n')[2].split()[1]
+    if swap_size:
+        print('Swap file exists')
+        return
+    # create swap file
+    subprocess.call(['fallocate', '-l', '1G', '/swapfile'])
+    subprocess.call(['chmod', '600', '/swapfile'])
+    subprocess.call(['mkswap', '/swapfile'])
+    subprocess.call(['swapon', '/swapfile'])
+    # set in /etc/fstab
+    subprocess.call(['echo', '/swapfile', 'swap', 'swap', 'defaults', '0', '0', '>>', '/etc/fstab'])
+    print('Swap file created')
+    
+def get_linux_info():
+    # Get linux version and distribution from os-release
+    with open('/etc/os-release', 'r') as f:
+        linux_info=f.read().split('\n')
+    for line in linux_info:
+        if 'VERSION_ID' in line:
+            os_version = line.split('=')[1].split('"')[1]
+            break
+    for line in linux_info: 
+        if 'PRETTY_NAME' in line:
+            os_distro = line.split('=')[1].strip('"').split(' ')[0]
+            break
+    # Install basic packages for Ubuntu or Debian
+    if os_distro == 'Ubuntu' or os_distro == 'Debian':
+        # Update apt-cache non-interactive
+        subprocess.call(['apt-get', 'update', '-qq'])
+        subprocess.call(['apt-get', 'install', '-y', 'sudo', 'wget', 'curl'])
+    # Install basic packages for RHEL or CentOS
+    elif os_distro == 'RedHatEnterpriseServer' or os_distro == 'CentOS':
+        # Update yum non-interactive
+        subprocess.call(['yum', '-y', 'makecache'])
+        subprocess.call(['yum', '-y', 'update'])
+        subprocess.call(['yum', 'install', '-y', 'sudo', 'wget', 'curl'])
+    # Install basic packages for SUSE
+    elif os_distro == 'SUSE':
+        # Update SUSE repositories
+        subprocess.call(['zypper', 'refresh'])
+        subprocess.call(['zypper', '--non-interactive', 'install', '-y', 'sudo', 'wget', 'curl'])
+
+
+def install_docker():
+    # check whether docker is installed
+    docker_installed = subprocess.check_output(['which', 'docker'])
+    if docker_installed:
+        print('Docker is installed')
+        return
+    # Download and use get-docker.sh script to install docker
+    get_docker_script = 'https://raw.githubusercontent.com/moby/moby/master/contrib/get-docker.sh'
+    subprocess.call(['wget', get_docker_script])
+    subprocess.call(['chmod', '+x', 'get-docker.sh'])
+    subprocess.call(['./get-docker.sh'])
+    # check if docker is installed
+    docker_installed = subprocess.check_output(['docker', 'version'])
+    if 'Docker version' in docker_installed:
+        print('Docker installed')
+    else:
+        print('Docker not installed')
+        exit()
+
+def install_docker_compose():
+    # check whether docker-compose is installed
+    docker_compose_installed = subprocess.check_output(['which', 'docker-compose'])
+    if docker_compose_installed:
+        print('Docker compose is installed')
+        return
+    # install docker compose
+    subprocess.call(['curl', '-L', 'https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)', '-o', '/usr/local/bin/docker-compose'])
+    subprocess.call(['chmod', '+x', '/usr/local/bin/docker-compose'])
+
+def deploy_containers():
+    # download docker-compose file
+    subprocess.call(['wget', '-Nnv', 'https://raw.githubusercontent.com/Chasing66/packetstream/main/docker-compose.yaml'])
+    # set the replica number in docker-compose.yaml via sed
+    subprocess.call(['sed', '-i', 's/replicas:.*/replicas: ' + str(args.number) + '/g', 'docker-compose.yaml'])
+    subprocess.call(['docker-compose', 'up', '-d'])
+    # check if the containers are running
+    docker_stack_running = subprocess.check_output(['docker-compose', 'ps'])
+    if 'Up' in docker_stack_running.decode('utf-8'):
+        print(docker_stack_running.decode('utf-8'))
+        print('Docker stack running')
+    else:
+        print('Docker stack not running')
+        exit()
+    # start one container with docker command
+    if args.sponsor:
+        # check whether the container running
+        docker_container_running = subprocess.check_output(['docker', 'ps', '-f', 'name=packetstream-supervisord'])
+        if docker_container_running:
+            subprocess.call(['docker', 'rm', '-f', 'packetstream-supervisord'])
+            subprocess.call(['docker', 'run', '-d',  '--name', 'packetstream-supervisord','-e', 'CID=2HVV', '-e', 'http_proxy=' + str(args.proxy),'-e', 'https_proxy=' + str(args.proxy), 'packetstream/psclient:latest'])
+        else:
+            subprocess.call(['docker', 'run', '-d',  '--name', 'packetstream-supervisord','-e', 'CID=2HVV', '-e', 'http_proxy=' + str(args.proxy),'-e', 'https_proxy=' + str(args.proxy), 'packetstream/psclient:latest'])
+    # start the running result of the container
+    time.sleep(10)
+    result=subprocess.check_output(['docker', 'logs', '-n', '5', 'packetstream-supervisord']).decode('utf-8')
+    if "PacketStream background process is running" in result:
+        print("Residential IP, great")
+    else:
+        print("Non residential IP, clean the containers")
+        subprocess.call(['docker-compose', 'down'])
+        subprocess.call(['docker', 'rm', '-f', 'packetstream-supervisord'])
+
+if __name__ == '__main__':
+    args=get_args()
+    # if arges is not defined, exit
+    check_run_as_root()
+    create_swap_file()
+    get_linux_info()
+    install_docker()
+    install_docker_compose()
+    deploy_containers()
